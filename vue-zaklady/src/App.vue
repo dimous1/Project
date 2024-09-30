a8ac2f5c29f062d33b9aa2213dcadf81a587d858
dimous1 <dmytrotsynyavskyy@seznam.cz>
2024-09-30 16:28:40 +0200

    first commit

---
+++ vue-zaklady/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Simple Counter</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increase">Increase</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Counter',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increase(): void {
+      this.count += 8
+    }
+  }
+})
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: orangered;
+  color: white;
+  border: none;
+}
+</style>
